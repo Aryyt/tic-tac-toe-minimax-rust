c23d35196da824a095fa749239f8be6477478a15
Aryyt <72268380+Aryyt@users.noreply.github.com>
2023-02-14 20:58:48 +0100

    Add files via upload

--- tic-tac-toe_MiniMax/src/board.rs
+++ tic-tac-toe_MiniMax/src/board.rs
@@ -1,28 +1,47 @@
+use core::num;
+use colored::Colorize;
+use std::collections::HashMap;
+use std::vec::Vec;
+
+
 
 pub struct Board{
-    state: [[i8; 3]; 3],
+    pub state: [[i8; 3]; 3],
     player_sign: i8,
     winner: i8,
+    squares: HashMap<usize,(usize,usize)>,
 } 
 
 impl Board {
     pub fn new(player_sign: i8) -> Self{
         let state:[[i8; 3]; 3]  = [[0; 3]; 3];
-        Self { state: state, player_sign: player_sign, winner: 0 }
+        let mut squares: HashMap<usize,(usize,usize)> = HashMap::new();
+        let mut key = 1;
+        for i in 0..state.len(){
+            for j in 0..state.len(){
+                squares.insert(key, (i,j));
+                key += 1;
+            }
+        }
+        Self { state: state, player_sign: player_sign, winner: 0, squares: squares}
     }
 
     pub fn print_board(&self){
+        let mut number = 1;
         for i in self.state{
             print!("-------------\n");
             for j in i {
                 if j == 0{
-                    print!("|   ");
+                    print!("| {} ", number);
+                    number += 1;
                 }
                 else if j == 1 {
-                    print!("| O ");
+                    print!("| {} ", "o".blue());
+                    number += 1;
                 }
                 else {
-                    print!("| X ");
+                    print!("| {} ", "x".red());
+                    number += 1;
                 }
             }
             print!("|\n");
@@ -31,23 +50,46 @@ impl Board {
 
     }
 
-    pub fn set_symbol(&mut self, x:usize, y:usize, symbol: i8 ){
-        self.state[x][y] = symbol; 
-    }
-
-    pub fn check_row_winner(&self, row: &[i8]) {
-        let first =true;
-        for i in row{
-
+    pub fn set_symbol(&mut self, suqare: usize, symbol: i8 ){
+        match self.squares.get(&suqare) {
+            None => println!("{} is not a valid square", suqare),
+            Some(cords) => {
+                self.state[cords.0][cords.1] = symbol; 
+            },
         }
     }
 
-    pub fn check_coll_winner(&self) {
-        
+    pub fn check_arr_for_winner(&self, arr: &[i8]) -> i8 {
+        let curr = arr[0];
+        if curr == 0{
+            return curr;
+        } 
+        else {
+            for i in 1..arr.len(){
+                if curr != arr[i] {
+                    return 0;
+                }
+            }
+            return curr;
+        }
     }
 
-    pub fn check_for_winner(&self){
+    pub fn get_colls(&self) -> [[i8; 3]; 3]{
+        let mut new_mat: [[i8; 3]; 3] = [[0; 3]; 3];
+        let mut nx = 0;
+        for y in 0..self.state.len() {
+            let mut ny = 0;
+            for x in 0..self.state.len(){
+                new_mat[nx][ny] = self.state[x][y] as i8;
+                ny += 1;
+            }
+            nx += 1;
+        }
+        return new_mat;
+    }
 
+    pub fn get_diag(&self) {
+        
     }
 
 }
--- tic-tac-toe_MiniMax/src/main.rs
+++ tic-tac-toe_MiniMax/src/main.rs
@@ -2,8 +2,19 @@ mod board;
 use board::*;
 fn main() {
     let mut board = Board::new(1);
-    board.set_symbol(1, 1, 1);
-    board.set_symbol(2, 2, -1);
+    board.set_symbol(1, 1);
+    board.set_symbol(3, -1);
+    board.set_symbol(2, 1);
     board.print_board();
+    let transpose = board.get_colls();
+    printArray(&transpose[0]);
+    let winner = board.check_arr_for_winner(&transpose[0]);
+    println!();
+    println!("{}", winner);
 }
 
+fn printArray(arr: &[i8]) {
+    for i in arr {
+        print!("{}", i);
+    }
+}
